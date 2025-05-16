63433c98eb0ca354d9bf0f446ab03f1c2bcdce25
Levi Bostian <levi@levi.earth>
2025-05-16 16:06:50 -0500

    build: modify so android app can include it locally for local development.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.android.library) apply false
-}
+
+// Do not place define plugins{} in this file. It breaks the ability for an android app to include this module locally in android studio.
